080f7246e66ef90fe32cf4efbb7bccc874b094d7
Shehla-Ibrar <shehlakayani98@gmail.com>
2025-01-19 10:29:23 +0500

    Update Binary-Search.py

--- Binary-Search.py
+++ Binary-Search.py
@@ -1,14 +1,14 @@
-def binary(array,target):
-  left,right=0,len(array)-1
-  while left<=right:
-    mid=(left+right)//2
-    if array[mid]==target:
+def binary(array,target): 
+  left,right=0,len(array)-1  #initial pointers
+  while left<=right:         #Loop to search
+    mid=(left+right)//2      #find mid index
+    if array[mid]==target:   #Target found
        return mid
-    elif array[mid]<target:
+    elif array[mid]<target:  #Search right half
        left=mid+1
-    else:
+    else:                    #Search left half  
       right=mid-1
-  return-1
+  return-1                   #Target Not found
   
   
 array=[10,20,30,40.50]
