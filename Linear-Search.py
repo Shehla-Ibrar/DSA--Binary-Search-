413892a5254dad2e01e678d1ea26671cd7c8f895
Shehla-Ibrar <shehlakayani98@gmail.com>
2025-01-19 10:34:12 +0500

    Update Linear-Search.py

--- Linear-Search.py
+++ Linear-Search.py
@@ -1,8 +1,9 @@
 def Linearsearch(array,target):
-    for i in range(len(array)):
-        if array[i]==target:
-            return i
-    return -1
+    for i in range(len(array)):    #Loop through each index of array
+        if array[i]==target:       #Check if current element matches the target
+
+            return i               #return the index if Target found
+    return -1                      #Target Not found
   
   
 array=[1,2,3,4,5]
